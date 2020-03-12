4d56b53d5e1f0b05ef50606db863b829be665e97
LinhTang <37064120+LinhTang@users.noreply.github.com>
2020-03-12 15:26:26 +0700

    add new files

---
+++ src/app/app.component.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+import { ChatAdapter } from "ng-chat";
+
+
+@Component({
+  selector: 'app-root',
+  templateUrl: './app.component.html',
+  styleUrls: ['./app.component.scss']
+})
+export class AppComponent {
+  title = 'client';
+}
